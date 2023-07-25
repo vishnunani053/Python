efceb0c2af069e44d36d220d3d1797da3c68aab1
vishnunani053 <vishnulucky053@gmail.com>
2023-07-25 22:52:11 +0530

    okkie

---
+++ assign-variables.py
@@ -0,0 +1,40 @@
+#Assinging values to variables
+#1.basic form
+str="hello world"
+print(str)
+
+#2.tuple assignment
+a,b=(10,20)
+print("a=",a)
+print("b=",b)
+
+#3.list assignment (using []brackets)
+x,y,z=[1,2,3]
+print(x)
+print(y)
+print(z)
+
+#4.sequence assignment
+a,b,c,d="nani"
+print(a)
+print(b)
+print(c)
+print(d)
+
+#5.extended sequence unpacking
+p,*q="hello"
+print(p)
+print(q)
+
+#6.multiplte target assignment
+
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+#Augmented assignment (updating the value)
+
+a=10
+a+=5
+print(a)
---
+++ demo.py
@@ -0,0 +1,8 @@
+a="helloooo"
+b=20
+c=5.4
+type(a)
+type(b)
+type(c)
+print(a)
+print(b)
